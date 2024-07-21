78be67f0743437993fb20a0d048cb8d1f0cf345a
Rohan Port <rohan.port@gmail.com>
2024-07-21 18:59:41 +1000

    Fix ups

--- install.sh
+++ install.sh
@@ -4,14 +4,12 @@
 # Install zsh and setup pure theme
 ########################################################################################################################
 
-cp ./.p10k.zsh ~/.p10k.zsh
-
 sudo apt-get update && sudo apt-get install -y curl vim
 sh -c "$(curl -L https://raw.githubusercontent.com/deluan/zsh-in-docker/master/zsh-in-docker.sh)" -- \
-    -a "export TERM=xterm-256color" \
-    -a "source ~/.p10k.zsh" # Override the zsh-in-docker term setting to allow for all styles of powerlevel10k
+    -a "export TERM=xterm-256color" # Override the zsh-in-docker term setting to allow for all styles of powerlevel10k
 
-# activate
-exec zsh
+cp ./.p10k.zsh ~/.p10k.zsh
+
+zshrc
 
 echo "Successfully installed zsh"
