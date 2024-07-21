06d804d16c0e9cc2055e442d270b44b5961ed468
Rohan Port <rohan.port@gmail.com>
2024-07-21 15:38:55 +1000

    Added install.sh

---
+++ install.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+########################################################################################################################
+# Install zsh and setup pure theme
+########################################################################################################################
+
+sudo apt-get update && sudo apt-get install -y curl
+sudo sh -c "$(curl -L https://raw.githubusercontent.com/deluan/zsh-in-docker/master/zsh-in-docker.sh)"
+
+mkdir -p ~/.zsh
+git clone https://github.com/sindresorhus/pure.git ~/.zsh/pure
+
+echo "# .zshrc
+fpath+=($HOME/.zsh/pure)" >> ~/.zshrc
+
+echo "# Pure color configs
+zstyle :prompt:pure:path color '35' # Pleasant green
+zstyle :prompt:pure:git:branch color '63' # Lavender
+zstyle :prompt:pure:prompt:success color '76' # Lime green
+
+# Load Pure as prompt
+autoload -U promptinit; promptinit
+prompt pure"  >> ~/.zshrc
