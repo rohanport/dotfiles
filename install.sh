1ced24268db8398ea820bc71419d1d7155537852
Rohan Port <rohan.port@gmail.com>
2024-07-21 18:35:30 +1000

    Install vim

--- install.sh
+++ install.sh
@@ -6,7 +6,7 @@
 
 cp ./.p10k.zsh ~/.p10k.zsh
 
-sudo apt-get update && sudo apt-get install -y curl
+sudo apt-get update && sudo apt-get install -y curl vim
 sh -c "$(curl -L https://raw.githubusercontent.com/deluan/zsh-in-docker/master/zsh-in-docker.sh)" -- \
     -a "export TERM=xterm-256color" \
     -a "source ~/.p10k.zsh" # Override the zsh-in-docker term setting to allow for all styles of powerlevel10k
