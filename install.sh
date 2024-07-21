14d16ff465c93321df29ff6d375fe8a9816b06d9
Rohan Port <rohan.port@gmail.com>
2024-07-21 18:16:36 +1000

    Explicitly specify 256 color term

--- install.sh
+++ install.sh
@@ -5,7 +5,8 @@
 ########################################################################################################################
 
 sudo apt-get update && sudo apt-get install -y curl
-sh -c "$(curl -L https://raw.githubusercontent.com/deluan/zsh-in-docker/master/zsh-in-docker.sh)"
+sh -c "$(curl -L https://raw.githubusercontent.com/deluan/zsh-in-docker/master/zsh-in-docker.sh)" -- \
+    -a "export TERM=xterm-256color" # Override the zsh-in-docker term setting to allow for all styles of powerlevel10k
 
 cp ./.p10k.zsh ~/.p10k.zsh
 
