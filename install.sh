8aad044bc8b923627acfb53e82d04f920114cdc9
Rohan Port <rohan.port@gmail.com>
2024-07-21 16:25:05 +1000

    Try pure theme using powerlevel10k

--- install.sh
+++ install.sh
@@ -5,21 +5,24 @@
 ########################################################################################################################
 
 sudo apt-get update && sudo apt-get install -y curl
-sudo sh -c "$(curl -L https://raw.githubusercontent.com/deluan/zsh-in-docker/master/zsh-in-docker.sh)" -t robbyrussell
+sudo sh -c "$(curl -L https://raw.githubusercontent.com/deluan/zsh-in-docker/master/zsh-in-docker.sh)"
 
-mkdir -p ~/.zsh
-git clone https://github.com/sindresorhus/pure.git ~/.zsh/pure
+git clone https://github.com/romkatv/powerlevel10k.git ~/powerlevel10k
+echo 'source ~/powerlevel10k/powerlevel10k.zsh-theme' >>! ~/.zshrc
 
-echo "# .zshrc
-fpath+=($HOME/.zsh/pure)" >> ~/.zshrc
+# mkdir -p ~/.zsh
+# git clone https://github.com/sindresorhus/pure.git ~/.zsh/pure
 
-echo "# Pure color configs
-zstyle :prompt:pure:path color '35' # Pleasant green
-zstyle :prompt:pure:git:branch color '63' # Lavender
-zstyle :prompt:pure:prompt:success color '76' # Lime green
+# echo "# .zshrc
+# fpath+=($HOME/.zsh/pure)" >> ~/.zshrc
 
-# Load Pure as prompt
-autoload -U promptinit; promptinit
-prompt pure"  >> ~/.zshrc
+# echo "# Pure color configs
+# zstyle :prompt:pure:path color '35' # Pleasant green
+# zstyle :prompt:pure:git:branch color '63' # Lavender
+# zstyle :prompt:pure:prompt:success color '76' # Lime green
+
+# # Load Pure as prompt
+# autoload -U promptinit; promptinit
+# prompt pure"  >> ~/.zshrc
 
 echo "Successfully installed zsh"
