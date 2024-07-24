46c264f3a3194a0c3869181f01812c3f591e3241
Rohan Port <rohan.port@gmail.com>
2024-07-24 10:52:55 +1000

    Persist .zsh_history between container rebuilds

--- install.sh
+++ install.sh
@@ -5,13 +5,17 @@
 ########################################################################################################################
 
 sudo apt-get update && sudo apt-get install -y curl vim
-sh -c "$(curl -L https://raw.githubusercontent.com/deluan/zsh-in-docker/master/zsh-in-docker.sh)" -- \
-    -a "export TERM=xterm-256color" # Override the zsh-in-docker term setting to allow for all styles of powerlevel10k
 
+# Force terminal to be support 256 colours so that we can use p10k pure style
+# Set HISTFILE to be in /commandhistory dir if exists so that we can persist between container rebuilds
+sh -c "$(curl -L https://raw.githubusercontent.com/deluan/zsh-in-docker/master/zsh-in-docker.sh)" -- \
+    -a "export TERM=xterm-256color" \
+    -a "if [ -d /commandhistory ]; && export HISTFILE=/commandhistory/.zsh_history"
+
+# Copy over p10k config
 cp ./.p10k.zsh ~/.p10k.zsh
 
 # Ensure new .p10k.zsh config in every terminal
-echo "# To customize prompt, run `p10k configure` or edit ~/.p10k.zsh.
 [[ ! -f ~/.p10k.zsh ]] || source ~/.p10k.zsh" >> ~/.zshrc
 
 echo "Successfully installed zsh"
