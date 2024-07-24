953fb9ab55b2777293e1ac4293cd75db6edebdef
Rohan Port <rohan.port@gmail.com>
2024-07-24 11:49:50 +1000

    Added sourcing of entrypoint.sh to .zshrc

--- install.sh
+++ install.sh
@@ -10,9 +10,11 @@ sudo apt-get update && sudo apt-get install -y curl vim
 
 # Force terminal to be support 256 colours so that we can use p10k pure style
 # Set HISTFILE to be in /commandhistory dir if exists so that we can persist between container rebuilds
+# Source the entrypoint.sh file if it exists
 sh -c "$(curl -L https://raw.githubusercontent.com/deluan/zsh-in-docker/master/zsh-in-docker.sh)" -- \
     -a "export TERM=xterm-256color" \
-    -a "[ -d /commandhistory ] && export HISTFILE=/commandhistory/.zsh_history"
+    -a "[ -d /commandhistory ] && export HISTFILE=/commandhistory/.zsh_history" \
+    -a "[ -f /entrypoint.sh ] && source /entrypoint.sh"
 
 # Enable Powerlevel10k instant prompt. Should stay close to the top of ~/.zshrc.
 # Initialization code that may require console input (password prompts, [y/n]
